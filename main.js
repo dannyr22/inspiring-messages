fdae7e037e10b8833ad323a6f616757c6cec9641
dannyr22 <dannyr2763@hotmail.com>
2020-10-29 16:10:50 +0000

    adding the arrays

---
+++ main.js
@@ -0,0 +1,5 @@
+const inspirationalMessages {
+    first: ['Never Give Up'],
+    second: ['Dont allow anybody anybody to bring you so low as to make you hate them'],
+    third: ['The night is always darkest just before the dawn']
+}
